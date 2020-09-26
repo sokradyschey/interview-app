18cea17b96021b795fe063fd307bab3b1b71bff7
Sokrady Chey <sokradychey@gmail.com>
2020-09-26 14:05:43 -0400

    Adjustment to jsx

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,36 +1,19 @@
 import React from 'react';
 import '../CSS/Header.css';
 
-// const Header = (
-//   <div className='Container'>
-//       <ul class="navigation">
-//         <li><a href="#">JavaScript</a></li>
-//         <li><a href="#">React</a></li>
-//         <li><a href="#">CSS</a></li>
-//         <li><a href="#">HTML</a></li>
-//       </ul>
-//           <div className='Nav'></div>
-//           <div className='Nav'></div>
-//           <div className='Nav'></div>
-//           <div className='Nav'></div>
-//       </div>
-// );
-
-function Header() {
-  return(
+class Header extends React.Component {
+  render() {
+    return(
       <div className='Container'>
-      <ul class="navigation">
-        <li><a href="#">JavaScript</a></li>
-        <li><a href="#">React</a></li>
-        <li><a href="#">CSS</a></li>
-        <li><a href="#">HTML</a></li>
-      </ul>
-          <div className='Nav'></div>
-          <div className='Nav'></div>
-          <div className='Nav'></div>
-          <div className='Nav'></div>
+        <ul class="navigation">
+          <li><a href="#">JavaScript</a></li>
+          <li><a href="#">React</a></li>
+          <li><a href="#">CSS</a></li>
+          <li><a href="#">HTML</a></li>
+        </ul>
       </div>
-  );
+    )
+  }
 }
 
 export default Header;
